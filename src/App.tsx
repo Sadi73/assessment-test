bf097fbd896ea7134e4868968080f17ec75205f6
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2025-04-22 15:38:05 +0600

    modified structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import { Route, Routes } from 'react-router'
-import './App.css'
-import Home from './components/Home'
 import FormRoot from './components/FormRoot'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/test" element={<FormRoot />} />
-    </Routes>
+    <div className=''>
+      <header className="text-center mb-12">
+        <h1 className="text-4xl font-bold mb-3 bg-gradient-to-r from-purple-500 to-indigo-500 text-transparent bg-clip-text">
+          Nimble Form Wizard
+        </h1>
+        <p className="text-muted-foreground max-w-2xl mx-auto">
+          A multi-step form that validates your inputs as you go, saves your progress,
+          and keeps a record of all your submissions.
+        </p>
+      </header>
+
+      <FormRoot />
+    </div>
   )
 }
 
